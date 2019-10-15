febad990caa3aa60704e67d934a8c484ed92207a
mj-raindays <killrain@gmail.com>
2019-10-15 12:35:54 +0900

    Update text

--- src/Components/Abstract/Abstract.js
+++ src/Components/Abstract/Abstract.js
@@ -4,7 +4,7 @@ import './Abstract.css';
 
 const Abstract = ({}) => (
   <div className="Abstract">
-    <h1>The serious business of fun and games on the blockchain</h1>
+    <h1>Games on the blockchain</h1>
     <div className="Desc">No jargon or nonsense. Just good fun.</div>
     <div className="Desc">Biscuitlabs는 바삭바삭하고 재미있는 블록체인 게임을 만들어 나갑니다.</div>
   </div>
