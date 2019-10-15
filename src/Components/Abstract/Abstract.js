26c0c86c468d0b506e99bc1d9fc63cbf8e8e2a64
mj-raindays <killrain@gmail.com>
2019-10-15 18:04:59 +0900

    Fix text

--- src/Components/Abstract/Abstract.js
+++ src/Components/Abstract/Abstract.js
@@ -8,7 +8,7 @@ const Abstract = ({}) => (
       <div className="AbstractBody">
         <h1>Games on the blockchain</h1>
         <div className="Desc">블록체인은 게임의 가능성을 확장시킬 것으로 믿습니다.</div>
-        <div className="Desc">Biscuitlabs는 블록체인을 가장 잘 확용한 게임을 만들어 나갑니다.</div>
+        <div className="Desc">Biscuitlabs는 블록체인을 가장 잘 활용해서 바삭바삭한 게임을 만들어 나갑니다.</div>
       </div>
       <div className="AbstractBgImgWrapper">
       </div>
